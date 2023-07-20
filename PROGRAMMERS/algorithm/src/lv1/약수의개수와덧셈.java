21a1b584d9380f830f3d9353181f1cb199773165
Cheol-Cheol <dream174822@gmail.com>
2023-07-20 14:19:33 +0900

    230720

--- PROGRAMMERS/algorithm/src/lv1/약수의개수와덧셈.java
+++ PROGRAMMERS/algorithm/src/lv1/약수의개수와덧셈.java
@@ -3,11 +3,24 @@ package lv1;
 public class 약수의개수와덧셈 {
     public int solution(int left, int right) {
         int answer = 0;
+
+        for (int num = left; num <= right; num++) {
+            int cnt = 0;
+
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) cnt++;
+            }
+
+            if (cnt % 2 == 0) {
+                answer += num;
+            } else answer -= num;
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
         약수의개수와덧셈 T = new 약수의개수와덧셈();
-        System.out.println(T.solution(13, 17));
+        System.out.println(T.solution(24, 27));
     }
 }
