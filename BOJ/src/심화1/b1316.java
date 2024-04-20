15884d87b2542f57c1e3c2c15eabb66806cde369
Cheol-Cheol <dream174822@gmail.com>
2024-04-20 20:43:25 +0900

    ✅ 240420

--- BOJ/src/심화1/b1316.java
+++ BOJ/src/심화1/b1316.java
@@ -9,22 +9,27 @@ public class b1316 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int cnt = N;
+        int cnt = 0;
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            boolean[] arr = new boolean[26];
-
-            for (int j = 0; j < str.length(); j++) {
-                if (arr[str.charAt(j) - 'a'] == false) {
-                    arr[str.charAt(j) - 'a'] = true;
-                } else if (j > 0 && str.charAt(j) != str.charAt(j - 1)) {
-                    cnt--;
-                    break;
-                }
+            if (isGroupString(br.readLine())) {
+                cnt++;
             }
         }
         System.out.println(cnt);
         br.close();
+    }
+
+    public static boolean isGroupString(String str) {
+        boolean[] alpha = new boolean[26];
+        for (int j = 0; j < str.length(); j++) {
+            int now = str.charAt(j);
+            int prev = j > 0 ? str.charAt(j - 1) : 0;
+            if (alpha[now - 'a'] == false) {
+                alpha[now - 'a'] = true;
+            } else if (now != prev) {
+                return false;
+            }
+        }
+        return true;
     }
 }
